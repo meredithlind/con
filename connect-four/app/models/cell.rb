5be236055322f460a8d55edc41a5f7db4f22f161
Meredith Lind <meredith@collectiveidea.com>
2017-11-18 10:01:01 -0500

    Add cell model and spec
    The cell will be used to keep track of the values of each spot on the
    board. The value defaults to 0, meaning it is empty. As a player makes a
    move the cell will be updated with a value of 1 or 2 to reflect who owns
    it.

---
+++ connect-four/app/models/cell.rb
@@ -0,0 +1,7 @@
+class Cell
+  attr_accessor :value
+
+  def initialize(value = 0)
+    @value = value
+  end
+end
---
+++ connect-four/spec/models/cell_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe Cell, type: :model do
+
+  context "#initialize" do
+    it "has a default value of 0" do
+      cell = Cell.new
+      expect(cell.value).to eq(0)
+    end
+
+    it "can be created with a value of 1" do
+      cell = Cell.new(1)
+      expect(cell.value).to eq(1)
+    end
+  end
+end
